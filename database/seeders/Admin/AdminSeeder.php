a7c631a1f592231decba12c91a0dc40a7d4204ef
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2026-04-22 21:49:34 +0800

    first commit

---
+++ database/seeders/Admin/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders\Admin;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = new Admin();
+        $admin->name = 'Superadmin';
+        $admin->email = 'superadmin@gmail.com';
+        $admin->password = bcrypt('1234');
+        $admin->save();
+    }
+}
